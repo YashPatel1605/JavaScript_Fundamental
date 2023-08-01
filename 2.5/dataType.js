b0e20af2e6cc29186a173a3381792cedb3958441
YashPatel1605 <139798781+YashPatel1605@users.noreply.github.com>
2023-08-01 15:32:29 +0530

    JavaScript Practice File

---
+++ 2.5/dataType.js
@@ -0,0 +1,24 @@
+//At a very High Level There are 2 Type of DataType 
+
+/*
+1]==>Primitive DataType ==>number,string,boolean,null,undefind,symbol(Unique dataType),Bigint
+2]==>reference DataType ==>object,array
+ */
+
+let a=100;
+console.log(typeof(a)); //number
+
+let fname="Yash";
+console.log(typeof(fname)); //string
+
+let value=true;
+console.log(typeof(value)); //boolean
+
+let sum=null;
+console.log(typeof(sum)); //null
+
+let b;
+console.log(typeof(b)); //undefind
+
+let x=128494727973038n;
+console.log(typeof(x)); //Bigint
