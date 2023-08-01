ebc354b15773ba30febfdfff4c76b2b6f78aa327
YashPatel1605 <139798781+YashPatel1605@users.noreply.github.com>
2023-08-01 15:49:25 +0530

    JavaScript Practice File

---
+++ 2.6/alert_prompt_confirm.js
@@ -0,0 +1,20 @@
+//1]===>alert
+
+//it show alert message in window Screen
+// It shows a message and waits for the user to press “OK”
+
+alert("This is a alert message");
+
+//2]===>prompt
+
+//The function prompt accepts two arguments
+//let message=prompt(titlt,[default]);
+// it will be null if cancle is pressed otherwise it will be user input
+let message=prompt("What Is Your name??","Yash");
+console.log(message);
+
+
+//3]===>confirm
+//this function show conformation from the user
+ let message1=confirm("Do You Like a JavaScript");
+ //if it is press ok the value will be true otherewise value is false
---
+++ 2.6/task1.js
@@ -0,0 +1,4 @@
+//Create a web-page that asks for a name and outputs it.
+
+let userName=prompt("Enter Your name","");
+alert(userName);
